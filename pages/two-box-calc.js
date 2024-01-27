dd463ea0674f0671cb53dd546dd22c36312fc509
mxw74 <105756082+mxw74@users.noreply.github.com>
2024-01-26 19:48:02 -0600

    get my inputs to work

--- pages/two-box-calc.js
+++ pages/two-box-calc.js
@@ -1,22 +1,44 @@
 import { Box, Button, Container, Heading, Input, SimpleGrid, Text } from "@chakra-ui/react"
 import { useState } from "react"
 
+// const SheaInput = () => {
+//   const [inputVal, setInputVal] = useState()
+//   const onChange = (evt) => {
+//     setInputVal(evt.target.value)
+//   }
 
-const InputOne = () => {
-  const [inputVal1, setInputVal1] = useState()
-  const [inputVal2, setInputVal2] = useState()
-  const onChange = (evt) => {
-    if (Input.input1 > 0 ) {
+//   return <>
+//     <Input {...{onChange}} />
+//     current val is: {inputVal}
+//   </>
+// }
+
+// const SheaInput2 = () => {
+//   const [inputVal, setInputVal] = useState()
+//   const onChange = (evt) => {
+//     setInputVal(evt.target.value)
+//   }
+
+//   return <>
+//     <Input {...{onChange}} />
+//     current val is: {inputVal}
+//   </>
+// }
+
+
+const NumInputs = ({inputVal1, inputVal2, setInputVal1, setInputVal2}) => {
+
+  const onChange1 = (evt) => {
     setInputVal1(evt.target.value)
-    } else {
+  }
+  const onChange2 = (evt) => {
     setInputVal2(evt.target.value)
-    }
   }
 
   return <>
-    <Input {...{onChange, className: 'input1', type: 'number'}} />
+    <Input {...{onChange: onChange1, type: 'number'}} />
     current val is: {inputVal1}
-    <Input {...{onChange, className: 'input2'}} />
+    <Input {...{onChange: onChange2}} />
     current val is: {inputVal2}
   </>
   // const [val, setVal] = useState()
@@ -47,7 +69,7 @@ const OppButn = ({onClick, oppSelected}) => {
 }
 
 
-const Opperators = ({onClick, value, oppSelected}) => {
+const Opperators = ({onClick, oppSelected}) => {
 
   return <Container {...{align: 'center'}}>
     <SimpleGrid {...{display: 'inline'}}>
@@ -60,15 +82,16 @@ const Opperators = ({onClick, value, oppSelected}) => {
 }
 
 
-export default function Calculator({val}) {
+export default function Calculator() {
+  const [inputVal1, setInputVal1] = useState()
+  const [inputVal2, setInputVal2] = useState()
+
   return <Container {...{bg:'pink', p: 5, color: 'black'}}>
     <Box {...{align: 'center', p: 5}}>
       <Heading>My Calculator</Heading>
       <Text>Type in the two numbers you want to calculate with then select an opperator to be displayed</Text>
-      <Box>{val}</Box>
     </Box>
-    <InputOne />
+    <NumInputs {...{inputVal1, inputVal2, setInputVal1, setInputVal2}} />
     <Opperators />
-    <InputTwo />
   </Container>
 }
