4c161bead4fd82756fb87e909d5e4af47c7fda0c
mxw74 <105756082+mxw74@users.noreply.github.com>
2024-03-05 14:23:26 -0600

    move forward with code and use states

--- pages/temp-button-conversion.js
+++ pages/temp-button-conversion.js
@@ -2,6 +2,10 @@ import { Button, Container, HStack, Heading, Input, Text } from "@chakra-ui/reac
 import { useState } from "react"
 import _ from "lodash"
 
+//Homework 
+// only 3 usestates 
+// highlight which button is selected (visually see which input & output is clicked)
+// remove complete calculation button (as long as input degrees and type and selected output temp type then it should just spit out the answer)
 
 const fahrenheitToCelsius = (f) => {
   return (f - 32) * (5/9)
@@ -31,55 +35,71 @@ const NumInput = ({placeholder, onChange}) => {
   return <Input {...{placeholder, onChange}}></Input>
 }
 
-const ConversionButton = ({onClick, orginVal, shadow}) => {
-  return <Button {...{onClick, colorScheme: 'facebook', variant: 'solid', border:'solid', m: 2, shadow, value: orginVal}}>{orginVal}</Button>
+const ConversionButton = ({onClick, unitVal, shadow}) => {
+  let colorScheme = 'facebook'
+  if (unitVal == 'Celsius') {
+    colorScheme = 'orange'
+  }
+  return <Button {...{onClick, colorScheme, variant: 'solid', border:'solid', m: 2, shadow, value: unitVal}}>{unitVal}</Button>
 }
 
 export default function TempButtonConversionCalc() {
-  const [val, setVal] = useState("")
-  const [degrees, setDegrees] = useState(0)
-  const [inputType, setInputType] = useState('')
-  const [outputType, setOutputType] = useState('') // added to set the output type of 
+  const [outputDegrees, setOutputDegrees] = useState()
+  const [inputDegrees, setInputDegrees] = useState(0)
+  const [inputType, setInputType] = useState('undefined units')
+  const [outputType, setOutputType] = useState('undefined units')
 
   const onChange1 = (evt) => {
-    setDegrees(_.toNumber(evt.target.value))
+    setInputDegrees(_.toNumber(evt.target.value))
   }
-
-  //i need to click the button and for it to register which button it clicked, take the text on the button, and move it to another location
-
-  const inputClick = (evt) => {
+  
+  const inputClick = (evt) => { 
     setInputType(evt.target.value)
-    setVal(degrees + ' degrees ' + inputType)
   }
 
   const outputClick = (evt) => {
-    setOutputType(evt.target.value) // I need this to set the string value that it is going to convert too on the second button (labled orginVal)
-    setVal(degrees + ' degrees ' + outputType) // I need this to know which conversion I want it to complete and take degrees as an input and put it into my conversion functions to put out a new output 
-    // I feel I need to use the useState && type function, but my brain cannot imagine how to use it atm
-    // I then need for the set val function to complete to conversion from the val that was taken in earlier to the output that converts that to the new designated value
-
+    // setOutputType(evt.target.value) // I need to move this and the if statements apart, but I cannot think of where to put the if statements
   }
 
+  const completeCalc = () => {
+    if (inputType == outputType) {
+      setOutputDegrees(inputDegrees)
+    } if (inputType == 'Celsius' && outputType == 'Fahrenheit') {
+      setOutputDegrees(celsiusToFahrenheit(inputDegrees))
+    } if (inputType == 'Celsius' && outputType == 'Kelvin') {
+      setOutputDegrees(celsiusToKelvin(inputDegrees))
+    } if (inputType == 'Fahrenheit' && outputType == 'Celsius') {
+      setOutputDegrees(fahrenheitToCelsius(inputDegrees))
+    } if (inputType == 'Fahrenheit' && outputType == 'Kelvin') {
+      setOutputDegrees(fahrenheitToKelvin(inputDegrees))
+    } if (inputType == 'Kelvin' && outputType == 'Fahrenheit') {
+      setOutputDegrees(kelvinToFarenheit(inputDegrees))
+    } if (inputType == 'Kelvin' && outputType == 'Celsius') {
+      setOutputDegrees(kelvinToCelsius(inputDegrees))
+    }
+  }
 
   return <Container {...{align: 'center'}}>
     <Heading>Conversion Calculator with Buttons</Heading>
     <NumInput {...{placeholder: 'Input goes here', onChange: onChange1}}/>
       <Container>
-        <ConversionButton {...{onClick: inputClick, orginVal: 'Celsius'}}/>
-        <ConversionButton {...{onClick: inputClick, orginVal: 'Fahrenheit'}}/>
-        <ConversionButton {...{onClick: inputClick, orginVal: 'Kelvin'}}/>
+        <ConversionButton {...{onClick: inputClick, unitVal: 'Celsius'}}/>
+        <ConversionButton {...{onClick: inputClick, unitVal: 'Fahrenheit'}}/>
+        <ConversionButton {...{onClick: inputClick, unitVal: 'Kelvin'}}/>
       </Container>
-      <Text>Your Input Value is set to: {val} </Text>
+      {inputDegrees && <Text>Your Input Value is set to: {inputDegrees} degrees {inputType}
+      </Text>}
       <Text>Select the conversion you would like to complete</Text>
       <Container>
-        <ConversionButton {...{onClick: outputClick, orginVal: 'Celsius'}}/> {/* These should not be orgin val versus output val or maybe these should just be val in general of any button to simplify this statement */}
-        <ConversionButton {...{onClick: outputClick, orginVal: 'Fahrenheit'}}/>
-        <ConversionButton {...{onClick: outputClick, orginVal: 'Kelvin'}}/>
+        <ConversionButton {...{onClick: outputClick, unitVal: 'Celsius'}}/> 
+        <ConversionButton {...{onClick: outputClick, unitVal: 'Fahrenheit'}}/>
+        <ConversionButton {...{onClick: outputClick, unitVal: 'Kelvin'}}/>
       </Container>
+      <Container> <Button {...{onClick: completeCalc}}>Complete Calculation</Button></Container>
     
 
     
-    <Text>Your output value has been converted to: {val}</Text>
+    <Text>Your output value has been converted to: {outputDegrees} degrees {outputType}</Text>
 
     
   </Container>
--- whatevs.mjs
+++ whatevs.mjs
@@ -1,9 +1,17 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 
-const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
 
-console.log(_.map(arr, (no) => {
-  return no * 2
-}))
+// console.log(_.map(arr, (no) => {
+//   return no * 2
+// }))
 
-console.log('hello')
+// console.log('hello')
+
+//truthy vs falsey
+//falsey = 0, empty string
+// const aaa = 'hello' //truthy
+const aaa = '' 
+if (aaa) {
+  console.log('success')  
+}
