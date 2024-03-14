aefe5b16dd68fb3c0d362508ac335922443097da
mxw74 <105756082+mxw74@users.noreply.github.com>
2024-03-14 12:49:18 -0500

    rough work

--- pages/temp-button-conversion.js
+++ pages/temp-button-conversion.js
@@ -5,85 +5,109 @@ import _ from "lodash"
 //Homework 
 // only 3 usestates 
 // highlight which button is selected (visually see which input & output is clicked)
-// remove complete calculation button (as long as input degrees and type and selected output temp type then it should just spit out the answer)
-
-const fahrenheitToCelsius = (f) => {
-  return (f - 32) * (5/9)
-}
-
-const fahrenheitToKelvin = (f) => {
-  return (f - 32) * 5/9 + 273.15
-}
-
-const celsiusToFahrenheit = (c) => {
-  return (c * 9/5) + 32
-}
-
-const celsiusToKelvin = (c) => {
-  return c + 273.15
-}
-
-const kelvinToCelsius = (k) => {
-  return k - 273
-}
-
-const kelvinToFarenheit = (k) => {
-  return (k - 273.15) * 1.8 + 32
-}
+// remove complete calculation button (as long as input degrees and type and selected output temp type then it should just spit out the answer) -- DONE 
 
 const NumInput = ({placeholder, onChange}) => {
-  return <Input {...{placeholder, onChange}}></Input>
+  return <Input {...{placeholder, onChange, name: 'numberInput'}}></Input>
 }
 
-const ConversionButton = ({onClick, unitVal, shadow}) => {
-  let colorScheme = 'facebook'
-  if (unitVal == 'Celsius') {
-    colorScheme = 'orange'
-  }
+const ConversionButton = ({onClick, unitVal, shadow, colorScheme}) => {
+  
+  // if (unitVal == 'Celsius') {
+  //   colorScheme = 'orange'
+  // }
   return <Button {...{onClick, colorScheme, variant: 'solid', border:'solid', m: 2, shadow, value: unitVal}}>{unitVal}</Button>
 }
 
+// The only use states I need are the user input ones 
+    // Input Degrees
+    // Input Type
+    // Output Type
+
 export default function TempButtonConversionCalc() {
-  const [outputDegrees, setOutputDegrees] = useState()
-  const [inputDegrees, setInputDegrees] = useState(0)
+  // const [outputDegrees, setOutputDegrees] = useState()
+  const [inputDegrees, setInputDegrees] = useState('')
   const [inputType, setInputType] = useState('undefined units')
   const [outputType, setOutputType] = useState('undefined units')
-
+  let outputDegrees = inputDegrees
   const onChange1 = (evt) => {
     setInputDegrees(_.toNumber(evt.target.value))
   }
   
   const inputClick = (evt) => { 
-    setInputType(evt.target.value)
+    setInputType(evt.target.value) // if I was able to change this to be a toggle for the color versus projecting the value on the scren then that could allow me to do the changes requested
+    // It would be a different usestate maybe a setColorScheme, isColorScheme
+    // might run into issues with output type 
+    // The only use states I need are the ones neccessary 
+        // 
   }
 
   const outputClick = (evt) => {
-    // setOutputType(evt.target.value) // I need to move this and the if statements apart, but I cannot think of where to put the if statements
+    setOutputType(evt.target.value)
+    completeCalc()
   }
 
-  const completeCalc = () => {
+  // would it be possible to set the output type and use the if statements to return the text? 
+
+  const completeCalc = () => { 
+
+    const fahrenheitToCelsius = (f) => {
+      return (f - 32) * (5/9)
+    }
+    
+    const fahrenheitToKelvin = (f) => {
+      return (f - 32) * 5/9 + 273.15
+    }
+    
+    const celsiusToFahrenheit = (c) => {
+      return (c * 9/5) + 32 
+    }
+    
+    const celsiusToKelvin = (c) => {
+      return c + 273.15
+    }
+    
+    const kelvinToCelsius = (k) => {
+      return k - 273
+    }
+    
+    const kelvinToFarenheit = (k) => {
+      return (k - 273.15) * 1.8 + 32
+    }
+
     if (inputType == outputType) {
-      setOutputDegrees(inputDegrees)
+        // setOutputDegrees(inputDegrees)
+        {inputDegrees && inputDegrees} // cannot get this one to work
     } if (inputType == 'Celsius' && outputType == 'Fahrenheit') {
-      setOutputDegrees(celsiusToFahrenheit(inputDegrees))
+        // return celsiusToFahrenheit(inputDegrees) + ' degrees Fahrenheit'
+        outputDegrees = celsiusToFahrenheit(outputDegrees)
+        // setOutputDegrees(celsiusToFahrenheit(inputDegrees))
     } if (inputType == 'Celsius' && outputType == 'Kelvin') {
-      setOutputDegrees(celsiusToKelvin(inputDegrees))
+        return celsiusToKelvin(inputDegrees)
+        // setOutputDegrees(celsiusToKelvin(inputDegrees))
     } if (inputType == 'Fahrenheit' && outputType == 'Celsius') {
-      setOutputDegrees(fahrenheitToCelsius(inputDegrees))
+        return fahrenheitToCelsius(inputDegrees)
+        // setOutputDegrees(fahrenheitToCelsius(inputDegrees))
     } if (inputType == 'Fahrenheit' && outputType == 'Kelvin') {
-      setOutputDegrees(fahrenheitToKelvin(inputDegrees))
+        return fahrenheitToKelvin(inputDegrees)
+        // setOutputDegrees(fahrenheitToKelvin(inputDegrees))
     } if (inputType == 'Kelvin' && outputType == 'Fahrenheit') {
-      setOutputDegrees(kelvinToFarenheit(inputDegrees))
+        return kelvinToFarenheit(inputDegrees)
+        // setOutputDegrees(kelvinToFarenheit(inputDegrees))
     } if (inputType == 'Kelvin' && outputType == 'Celsius') {
-      setOutputDegrees(kelvinToCelsius(inputDegrees))
+        return kelvinToCelsius(inputDegrees)
+        // setOutputDegrees(kelvinToCelsius(inputDegrees))
     }
   }
 
   return <Container {...{align: 'center'}}>
     <Heading>Conversion Calculator with Buttons</Heading>
-    <NumInput {...{placeholder: 'Input goes here', onChange: onChange1}}/>
+    <HStack>
+      <NumInput {...{placeholder: 'Input goes here', onChange: onChange1}}/> 
+      <Text {...{w: '50%'}}>Degrees</Text>
+    </HStack>
       <Container>
-        <ConversionButton {...{onClick: inputClick, unitVal: 'Celsius'}}/>
+        <ConversionButton {...{onClick: inputClick, unitVal: 'Celsius', colorScheme: 'facebook'}}/>
         <ConversionButton {...{onClick: inputClick, unitVal: 'Fahrenheit'}}/>
         <ConversionButton {...{onClick: inputClick, unitVal: 'Kelvin'}}/>
       </Container>
@@ -94,14 +118,10 @@ export default function TempButtonConversionCalc() {
         <ConversionButton {...{onClick: outputClick, unitVal: 'Celsius'}}/> 
         <ConversionButton {...{onClick: outputClick, unitVal: 'Fahrenheit'}}/>
         <ConversionButton {...{onClick: outputClick, unitVal: 'Kelvin'}}/>
-      </Container>
-      <Container> <Button {...{onClick: completeCalc}}>Complete Calculation</Button></Container>
-    
-
-    
-    <Text>Your output value has been converted to: {outputDegrees} degrees {outputType}</Text>
-
-    
+      </Container>     
+    {outputType == 'Celsius' && <Text> {outputDegrees} {/*I want it to complete the calculation right here */} {outputType}</Text>} 
+    {outputType == 'Fahrenheit' && <Text>{outputDegrees} {outputType}</Text>} 
+    {outputType == 'Kelvin' && <Text>{outputDegrees} {outputType}</Text>}    
   </Container>
 }
 
